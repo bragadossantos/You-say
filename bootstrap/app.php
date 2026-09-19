40ea4c011c1a97cee4fab25579965cf23c8d9586
Braga Dos Santos <braga.kauimbi@gmail.com>
2026-09-19 15:42:41 +0100

    fix: isentar rotas web da verificacao CSRF para resolver 419 Page Expired na Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            '*',
+        ]);
         $middleware->alias([
             'admin' => EnsureUserIsAdmin::class,
         ]);
